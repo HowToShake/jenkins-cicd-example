4bca8a597919382e1c8d402b5ebbdc5d83f93144
Michał Ryśkiewicz <ryskiewicz.m@gmail.com>
2021-06-26 00:11:59 +0200

    [ADDED] new endpoint

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Guys!!');
 });
+
+app.get('/api', (req, res) => {
+  res.send('Hello API!!');
+});
+
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
